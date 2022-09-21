9138e47dd81c2181fa76aa46b61f286ba09bf64f
1E10 Laptop <jzeng6527@bths.edu>
2022-09-21 12:15:28 -0400

    Added a method that will ask for user input(s).

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi.");
+    }
+
+    public static void extra(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?: ");
+        String name = scan.nextLine();
+        scan.nextLine();
+
+        System.out.println("What is your favorite color?: ");
+        String favColor = scan.nextLine();
+        scan.nextLine();
+
+        System.out.println("What is your dream pet?: ");
+        String animal = scan.nextLine();
+        scan.nextLine();
+
+        System.out.println("What is your favorite number? (whole or other): ");
+        double favNum = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("");
+    }
 }
